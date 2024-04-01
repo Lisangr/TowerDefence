857f38c5bfd2d9031eb484745a3105f409e05eda
Lisangr <iliyayefimov@gmail.com>
2024-04-01 21:41:27 +0300

    Camera Move

--- Others/CameraController.cs
+++ Others/CameraController.cs
@@ -1,43 +1,209 @@
+/*using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform targetPoint;
+    public float speed;
+    public GestureClick gestureClick;
+
+    private Transform selfTransform;
+
+    private void Start()
+    {
+        selfTransform = GetComponent<Transform>();
+        gestureClick.OnClick += (pos) =>
+        {
+            targetPoint.position = pos;
+        };
+    }
+
+    void Update()
+    {
+       selfTransform.position = Vector3.MoveTowards(selfTransform.position, 
+                                                    targetPoint.position, 
+                                                    speed * Time.deltaTime);
+    }
+}*/
+/*
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
-    private Camera camera;
-    private Vector3 startPosition;
-
-    private float targetPositionX;
-    private float targetPositionY;
-    private float targetPositionZ;
+    public Transform targetPoint;
     public float speed;
+    public GestureClick gestureClick;
 
-    void Awake()
+    private Transform selfTransform;
+    private bool isRightClicking = false;
+
+    private void Start()
     {
-        camera = GetComponent<Camera>();
+        selfTransform = GetComponent<Transform>();
+        gestureClick.OnClick += (pos) =>
+        {
+            targetPoint.position = new Vector3(pos.x, targetPoint.position.y, pos.z);
+        };
+    }
+
+    void Update()
+    {
+        selfTransform.position = Vector3.MoveTowards(selfTransform.position,
+                                                    targetPoint.position,
+                                                    speed * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            isRightClicking = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            isRightClicking = false;
+        }
+
+        if (isRightClicking)
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            selfTransform.RotateAround(targetPoint.position, Vector3.up, mouseX * speed);
+        }
+    }
+}*//*
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed;
+    public Terrain terrain;
+    public Transform targetPoint;
+
+    private Transform selfTransform;
+    private bool isMoving = false;
+    private bool isRightClicking = false;
+
+    private void Start()
+    {
+        selfTransform = GetComponent<Transform>();
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            startPosition = camera.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (terrain.GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity))
+            {
+                isMoving = true;
+            }
         }
-        else if (Input.GetMouseButton(0))
+
+        if (Input.GetMouseButtonUp(0))
         {
-            float positionX = camera.ScreenToWorldPoint(Input.mousePosition).x - startPosition.x;
-            float positionZ = camera.ScreenToWorldPoint(Input.mousePosition).z - startPosition.z;
-            float positionY = camera.ScreenToWorldPoint(Input.mousePosition).y - startPosition.y;
-
-            targetPositionX = Mathf.Clamp(transform.position.x - positionX, 0f, 1f);
-            targetPositionZ = Mathf.Clamp(transform.position.z - positionZ, 0f, 1f);
-            targetPositionY = Mathf.Clamp(transform.position.y - positionY, 0f, 1f);
+            isMoving = false;
         }
 
-        transform.position = new Vector3(
-            Mathf.Lerp(transform.position.x, targetPositionX, speed * Time.deltaTime),
-            Mathf.Lerp(transform.position.y, targetPositionY, speed * Time.deltaTime),
-            Mathf.Lerp(transform.position.z, targetPositionZ, speed * Time.deltaTime)
-        );
+        if (isMoving)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (terrain.GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity))
+            {
+                Vector3 targetPosition = hit.point;
+                targetPosition.y = selfTransform.position.y; // Keep the same Y position
+                selfTransform.position = Vector3.MoveTowards(selfTransform.position, targetPosition, speed * Time.deltaTime);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            isRightClicking = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            isRightClicking = false;
+        }
+
+        if (isRightClicking)
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y"); 
+            selfTransform.RotateAround(targetPoint.position, Vector3.up, mouseX * speed);
+            selfTransform.RotateAround(targetPoint.position, selfTransform.right, -mouseY * speed); 
+        }
+    }
+}*/
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed;
+    public float zoomSpeed;
+    public float minZoomDistance;
+    public float maxZoomDistance;
+    public Transform targetPoint;
+
+    private Transform selfTransform;
+    private bool isMoving = false;
+    private bool isRightClicking = false;
+
+    private void Start()
+    {
+        selfTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMoving = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isMoving = false;
+        }
+
+        if (isMoving)
+        {
+            Vector3 targetPosition = GetTargetPosition();
+            targetPosition.y = selfTransform.position.y; // Keep the same Y position
+            selfTransform.position = Vector3.MoveTowards(selfTransform.position, targetPosition, speed * Time.deltaTime);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            isRightClicking = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            isRightClicking = false;
+        }
+
+        if (isRightClicking)
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+            selfTransform.RotateAround(targetPoint.position, Vector3.up, mouseX * speed);
+            selfTransform.RotateAround(targetPoint.position, selfTransform.right, -mouseY * speed);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 zoomDirection = selfTransform.position - targetPoint.position;
+        float zoomDistance = zoomDirection.magnitude;
+        zoomDistance = Mathf.Clamp(zoomDistance - scroll * zoomSpeed, minZoomDistance, maxZoomDistance);
+        selfTransform.position = targetPoint.position + zoomDirection.normalized * zoomDistance;
+    }
+
+    private Vector3 GetTargetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, targetPoint.position);
+        float distance;
+        if (plane.Raycast(ray, out distance))
+        {
+            return ray.GetPoint(distance);
+        }
+        return selfTransform.position;
     }
 }
-
---
+++ Others/GestureClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+public class GestureClick : MonoBehaviour, IPointerClickHandler
+{
+    public UnityAction<Vector3> OnClick;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnClick.Invoke(eventData.pointerPressRaycast.worldPosition);
+    }
+}
